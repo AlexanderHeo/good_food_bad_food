83971818d812c9c70eefe6fb28471b6810cf6e5f
Your Namethefullm0nty <you@example.commontgomery.evan@gmail.com>
2020-01-22 20:54:53 +0000

    added spaces

--- server/public/ajax.js
+++ server/public/ajax.js
@@ -12,16 +12,22 @@ function getFDAData(event) {
 
       const FDAWarning = response.meta.disclaimer;
       console.log('WARNING: ', FDAWarning);
+
       const FDADateIssued = response.results[0].report_date;
       console.log('Date issued: ', FDADateIssued);
+
       const FDACity = response.results[0].city;
       console.log('City: ', FDACity);
+
       const FDAReason = response.results[0].reason_for_recall;
       console.log('Reason: ', FDAReason);
+
       const FDADescription = response.results[0].product_description;
       console.log('Product: ', FDADescription);
+
       const FDAQuantity = response.results[0].product_quantity;
       console.log('Quantity: ', FDAQuantity);
+
       const FDADistribution = response.results[0].distribution_pattern;
       console.log(FDADistribution);
     },
