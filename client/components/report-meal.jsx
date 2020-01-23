1836114ea023194c3e4e0226af7a42de804412ce
Your Namethefullm0nty <you@example.commontgomery.evan@gmail.com>
2020-01-23 19:41:23 +0000

    started creating react class

--- client/components/report-meal.jsx
+++ client/components/report-meal.jsx
@@ -1,27 +1,27 @@
-import React from 'react'
+import React from 'react';
 
 export default class ReportMeal extends React.Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
   }
 
   // componentDidMount(){
 
   // }
 
-  render(){
+  render() {
     return (
       <div className={'container'}>
         <div className={'EnterEffectDiv'}></div>
         <div>Choose the food you want to enter effect for:</div>
-        <ul class="list-group">
-          <li class="list-group-item active">Food One</li>
-          <li class="list-group-item">Food Two</li>
-          <li class="list-group-item">Food Three</li>
-          <li class="list-group-item">Food Four</li>
-          <li class="list-group-item">Food Five</li>
+        <ul className="list-group">
+          <li className="list-group-item active">Food One</li>
+          <li className="list-group-item">Food Two</li>
+          <li className="list-group-item">Food Three</li>
+          <li className="list-group-item">Food Four</li>
+          <li className="list-group-item">Food Five</li>
         </ul>
       </div>
-    )
+    );
   }
 }
--- client/index.jsx
+++ client/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
-import ReportMeal from './components/report-meal'
+import ReportMeal from './components/report-meal';
 
 ReactDOM.render(
   <App />,
