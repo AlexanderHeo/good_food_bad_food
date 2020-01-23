e8507260f0f0814b09c9bffdd25ebe156d7fa4fa
Your Namethefullm0nty <you@example.commontgomery.evan@gmail.com>
2020-01-23 21:12:49 +0000

    basic front end skeleton created for choose meal and rate meal

--- client/index.jsx
+++ client/index.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
-import ReportMeal from './components/report-meal';
+
 
 ReactDOM.render(
   <App />,
-  <ReportMeal />,
   document.querySelector('#root')
 );
