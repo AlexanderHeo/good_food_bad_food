ce0904835a17d7d0e43796c34bda0b63db3dc934
Your Namethefullm0nty <you@example.commontgomery.evan@gmail.com>
2020-01-24 01:39:21 +0000

    data coming back and getting mapped correctly.  Need to get data into divs.

--- client/components/rate-meal.jsx
+++ client/components/rate-meal.jsx
@@ -26,7 +26,7 @@ export default class RateMeal extends React.Component {
       })
       .then(data => {
         this.setState({ meals: data });
-        console.log('data: ', this.state.meals);
+        // console.log('meals: ', this.state.meals);
       })
       .catch(err => {
         alert('Error: ', err);
@@ -34,63 +34,70 @@ export default class RateMeal extends React.Component {
   }
 
   render() {
-    // const meals = this.state.meals
-    // const mealsMap = meals.map(mealObject => {
-    //   console.log("meals map: ", mealsMap)
-    //   console.log("meals object: ", mealObject)
-    // })
-    if (this.state.rateFood === false) {
-      return (
-        <div className={'container'} style={{ width: '375px', height: '667px' }}>
-          <div className={'enterEffectDiv'}></div>
-          <div className={'bg-primary'}>Choose the food you want to enter effect for:</div>
-          <div className="list-group">
-            <a href="#" className="list-group-item list-group-item-action" onClick={this.handleFoodClick}>Tomato Soup
+    if (this.state.meals.length === 0){
+      return null
+    } else {
+      if (this.state.rateFood === false) {
+        const allMeals = this.state.meals
+        console.log("meal state in render: ", allMeals)
+        // console.log("meal object: ", mealObject)
+        allMeals.map(mealObject => {
+          console.log(mealObject)
+        })
+        return (
+          <div className={'container'} style={{ width: '375px', height: '667px' }}>
+            <div className={'enterEffectDiv'}></div>
+            <div className={'bg-primary'}>Choose the food you want to enter effect for:</div>
+            <div className="list-group">
+              <a href="#" className="list-group-item list-group-item-action" onClick={this.handleFoodClick}>Pizza
               <img src='./images/badFace.jpg' style={{ width: '25px', height: '25px', float: 'right' }}></img>
-            </a>
-            <a href="#" className="list-group-item list-group-item-action" onClick={this.handleFoodClick}>World's Hottest Wings
+              </a>
+              <a href="#" className="list-group-item list-group-item-action" onClick={this.handleFoodClick}>Hot Dog
               <img src='./images/happyFace.jpg' style={{ width: '25px', height: '25px', float: 'right' }}></img>
-            </a>
-            <a href="#" className="list-group-item list-group-item-action" onClick={this.handleFoodClick}>Pepperoni and Jalapeno Pizza
+              </a>
+              <a href="#" className="list-group-item list-group-item-action" onClick={this.handleFoodClick}>Hamburger
               <img src='./images/happyFace.jpg' style={{ width: '25px', height: '25px', float: 'right' }}></img>
-            </a>
-            <a href="#" className="list-group-item list-group-item-action" onClick={this.handleFoodClick}>PB & J Sandwich
+              </a>
+              <a href="#" className="list-group-item list-group-item-action" onClick={this.handleFoodClick}>Salad
               <img src='./images/neutralFace.jpg' style={{ width: '25px', height: '25px', float: 'right' }}></img>
-            </a>
-            <a href="#" className="list-group-item list-group-item-action" onClick={this.handleFoodClick}>Chex Mix
+              </a>
+              <a href="#" className="list-group-item list-group-item-action" onClick={this.handleFoodClick}>Hot Wings
               <img src='./images/neutralFace.jpg' style={{ width: '25px', height: '25px', float: 'right' }}></img>
-            </a>
+              </a>
+            </div>
+            <button className={'.doneButton'} style={{ float: 'left' }}>
+              <span>
+                Done
+            </span>
+            </button>
+            <button className={'.cancelButton'} style={{ float: 'right' }}>
+              <span>
+                Cancel
+            </span>
+            </button>
           </div>
-          <button className={'.doneButton'} style={{ float: 'left' }}>
-            <span>
-              Done
-            </span>
-          </button>
-          <button className={'.cancelButton'} style={{ float: 'right' }}>
-            <span>
-              Cancel
-            </span>
-          </button>
-        </div>
-      );
-    } else if (this.state.rateFood === true) {
-      return (
-        <div className={'container'} style={{ width: '375px', height: '667px' }}>
-          <div className={'bg-warning'}>How did it make you feel?</div>
-          <div className={'bg-info'}>FOOD ITEM NAME</div>
-          <div className="list-group">
-            <a href="#" className="list-group-item list-group-item-action" onClick={this.handleRatingClick}>Good
+        );
+      } else if (this.state.rateFood === true) {
+        return (
+          <div className={'container'} style={{ width: '375px', height: '667px' }}>
+            <div className={'bg-warning'}>How did it make you feel?</div>
+            <div className={'bg-info'}>FOOD ITEM NAME</div>
+            <div className="list-group">
+              <a href="#" className="list-group-item list-group-item-action" onClick={this.handleRatingClick}>Good
               <img src='./images/happyFace.jpg' style={{ width: '25px', height: '25px', float: 'right' }}></img>
-            </a>
-            <a href="#" className="list-group-item list-group-item-action" onClick={this.handleRatingClick}>Ok
+              </a>
+              <a href="#" className="list-group-item list-group-item-action" onClick={this.handleRatingClick}>Ok
               <img src='./images/neutralFace.jpg' style={{ width: '25px', height: '25px', float: 'right' }}></img>
-            </a>
-            <a href="#" className="list-group-item list-group-item-action" onClick={this.handleRatingClick}>Bad
+              </a>
+              <a href="#" className="list-group-item list-group-item-action" onClick={this.handleRatingClick}>Bad
               <img src='./images/badFace.jpg' style={{ width: '25px', height: '25px', float: 'right' }}></img>
-            </a>
+              </a>
+            </div>
           </div>
-        </div>
-      );
+        );
+      }
     }
-  }
+    }
+
+
 }
