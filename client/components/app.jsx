0b75647cb962ed9312d3936717a5fee21abe94bf
Andrew Zhou <andersonzhoutree@gmail.com>
2020-01-24 21:16:26 +0000

    fixed dev

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,55 +1,5 @@
 import React from 'react';
 
-import Enter from './enter.jsx';
-import ListMeals from './listEnterFood.jsx';
 import UserMealStatus from './user-meals-status';
 
-
-class EnterFoodScreen extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = ({
-      todaysMeals: []
-    });
-    this.addMeal = this.addMeal.bind(this);
-  }
-
-  addMeal(newMeal) {
-    fetch('/api/enter', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(newMeal)
-    })
-      .then(response => {
-        return response.json();
-      })
-      .then(jsonData => {
-        const todaysMealsCopy = [...this.state.todaysMeals];
-        const addedMeal = todaysMealsCopy.concat(jsonData.name);
-        this.setState({
-          todaysMeals: addedMeal
-        });
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }
-
-  render() {
-    return (
-      <>
-        <Enter
-          onSubmit={ this.addMeal }
-          todaysMeals={ this.state.todaysMeals }
-        />
-        <ListMeals
-          todaysMeals={ this.state.todaysMeals }
-        />
-      </>
-    );
-  }
-}
-
-export default EnterFoodScreen;
+export default App;
