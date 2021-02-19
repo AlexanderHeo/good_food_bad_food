c50a067189ad563af1ebf232e3f10e22b28a8b4c
Alexander Heo <jikster712@gmail.com>
2021-02-19 14:51:25 -0800

    todays-meals component redesign - mealtime, food, rating / mealtime

--- client/components/Todays-Meals.jsx
+++ client/components/Todays-Meals.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
+import Loader from './Loader'
+import TodaysMealItem from './Todays-Meal-Item'
 
 class TodaysMeals extends Component {
 	state = {
 	  breakfastReady: false,
 	  lunchReady: false,
 	  dinnerReady: false,
-	  snackReady: false,
+	  snacksReady: false,
 	  breakfast: [],
 	  lunch: [],
 	  dinner: [],
@@ -16,23 +18,28 @@ class TodaysMeals extends Component {
 
 	componentDidMount() {
 	  const { todaysMeals } = this.props
-	  const breakfast = todaysMeals.filter(x => x.mealtime === 'breakfast')
-	  const lunch = todaysMeals.filter(x => x.mealtime === 'lunch')
-	  const dinner = todaysMeals.filter(x => x.mealtime === 'dinner')
-	  const snacks = todaysMeals.filter(x => x.mealtime === 'snacks')
+	  const breakfast = todaysMeals.filter(x => x.mealtime === 'b')
+	  const lunch = todaysMeals.filter(x => x.mealtime === 'l')
+	  const dinner = todaysMeals.filter(x => x.mealtime === 'd')
+	  const snacks = todaysMeals.filter(x => x.mealtime === 's')
 
 	  this.setState({
-	    breakfastReady: true,
-	    lunchReady: true,
-	    dinnerReady: true,
-	    snackReady: true,
-	    breakfast: breakfast,
-	    lunch: lunch,
-	    dinner: dinner,
-	    snacks: snacks,
-	    ready: true
+	    breakfast: breakfast[0],
+	    lunch: lunch[0],
+	    dinner: dinner[0],
+	    snacks: snacks[0],
+	    ready: true,
+	    breakfastReady: true
 	  })
+	}
 
+	handleMealTimeClick = mealtime => {
+	  fetch('/api/enter')
+	    .then(response => response.json())
+	    .then(result => {
+	      // console.log(result)
+	    })
+	    .catch(err => console.error(err))
 	}
 
 	render() {
@@ -41,44 +48,41 @@ class TodaysMeals extends Component {
 	      {
 	        this.state.ready
 	          ? <>
-	            <div className='breakfast meal'>
-	              <span className='mealName' >Breakfast:</span>
-	              {
-	                this.state.breakfastReady
-	                  ? <span>{ this.state.breakfast[0].name }</span>
-	                  : null
-	              }
-	              <span className='mealRating' ><div></div></span>
-	            </div>
-	            <div className='lunch meal' >
-	              <span className='mealName' >Lunch</span>
-	              {
-	                this.state.lunchReady
-	                  ? <span>{ this.state.lunch[0].name }</span>
-	                  : null
-	              }
-	              <span className='mealRating' ><div></div></span>
-	            </div>
-	            <div className='dinner meal' >
-	              <span className='mealName' >Dinner</span>
-	              {
-	                this.state.dinnerReady
-	                  ? <span>{ this.state.dinner[0].name }</span>
-	                  : null
-	              }
-	              <span className='mealRating' ><div></div></span>
-	            </div>
-	            <div className='snacks meal' >
-	              <span className='mealName' >Snacks</span>
-	              {
-	                this.state.snacksReady
-	                  ? <span>{ this.state.snacks[0].name }</span>
-	                  : null
-	              }
-	              <span className='mealRating' ><div></div></span>
-	            </div>
+	            {
+	              this.state.breakfastReady
+	                ? <TodaysMealItem
+	                  food={ this.state.breakfast }
+	                  mealtime='breakfast' />
+	                : <div
+	                  onClick={ () => this.handleMealTimeClick('breakfast') }
+	                  className='meal mealTime'>Breakfast</div>
+	            }
+	            {
+	              this.state.lunchReady
+	                ? <TodaysMealItem
+	                  food={ this.state.lunch } />
+	                : <div
+	                  onClick={ () => this.handleMealTimeClick('lunch') }
+	                  className='meal mealTime'>Lunch</div>
+	            }
+	            {
+	              this.state.dinnerReady
+	                ? <TodaysMealItem
+	                  food={ this.state.dinner } />
+	                : <div
+	                  onClick={ () => this.handleMealTimeClick('dinner') }
+	                  className='meal mealTime'>Dinner</div>
+	            }
+	            {
+	              this.state.snacksReady
+	                ? <TodaysMealItem
+	                  food={ this.state.snacks } />
+	                : <div
+	                  onClick={ () => this.handleMealTimeClick('snacks') }
+	                  className='meal mealTime'>Snacks</div>
+	            }
 	          </>
-	          : null
+	          : <Loader />
 	      }
 	    </Container>
 	  )
@@ -94,28 +98,18 @@ const Container = styled.div`
 		height: 25%;
 		display: flex;
 		align-items: center;
-		.mealName {
-			width: 50%;
-			margin-left: 20px;
-			display: flex;
-			justify-content: center;
-		}
-		.mealRating {
-			width: 50%;
-			display: flex;
-			justify-content: center;
-			div {
-				width: 20px;
-				height: 20px;
-				border: 10px solid red;
-				border-radius: 50px;
-			}
-		}
 	}
 	.meal:nth-child(even) {
 		background-color: var(--primary-1);
 	}
 	.meal:nth-child(odd) {
 		background-color: var(--primary-0);
+	}
+	.mealTime {
+		font-size: 1.6rem;
+		font-weight: 700;
+		display: flex;
+		justify-content: center;
+		align-items: center;
 	}
 `
