2abd3289b9dc06751f3a3da180addde1c688a746
Alexander Heo <jikster712@gmail.com>
2021-02-23 13:33:12 -0800

    added Iconify, redesigned Todays Meal Item

--- client/components/Todays-Meal-Item.jsx
+++ client/components/Todays-Meal-Item.jsx
@@ -2,18 +2,28 @@ import React from 'react'
 import styled from 'styled-components'
 
 const TodaysMealItem = props => {
+  const first = props.mealtime.charAt(0).toUpperCase()
   return (
     <Container
       onClick={ () => props.handleClick('edit', props.mealtime)}
       className="meal">
       <div className='mealName'>
-        <span>{ props.mealtime }:</span>
+        <span>
+          <div className='outerRing'>
+            <div className='innerRing'>
+              <span className='first'>{ first }</span>
+            </div>
+          </div>
+        </span>
       </div>
       <div className='foodItems'>
-        <span>{ props.food.name }</span>
+        <span className='food'>{ props.food.name }</span>
       </div>
       <div className='mealRating'>
         <span>{ props.food.report }</span>
+      </div>
+      <div className='buttonContainer'>
+	      <span className='iconify' data-icon="octicon-trashcan" data-inline='false' />
       </div>
     </Container>
   )
@@ -22,6 +32,27 @@ const TodaysMealItem = props => {
 export default TodaysMealItem
 
 const Container = styled.button`
+
+	.first {
+		font-size: 1.8rem;
+		font-weight: 700;
+	}
+	.outerRing, .innerRing {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		border-radius: 50%;
+	}
+	.outerRing {
+		width: 44px;
+		height: 44px;
+		background-color: var(--primary-6);
+	}
+	.innerRing {
+		width: 36px;
+		height: 36px;
+		background-color: var(--primary-0);
+	}
 	.mealName, .foodItems, .mealRating {
 		height: 100%;
 		display: flex;
@@ -34,7 +65,18 @@ const Container = styled.button`
 	.foodItems {
 		width: 60%;
 	}
+	.foodItems .food {
+		font-size: 1.4rem;
+	}
 	.mealRating {
 		width: 20%;
+	}
+
+	.buttonContainer {
+		display: flex;
+		.iconify {
+			width: 38px;
+			height: 38px;
+		}
 	}
 `
