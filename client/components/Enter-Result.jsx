45a6b5e2c8d1ba3258016e46e662d76beb6a3a85
Alexander Heo <jikster712@gmail.com>
2021-02-22 19:48:22 -0800

    extracted all icons into exported components

--- client/components/Enter-Result.jsx
+++ client/components/Enter-Result.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
-
+import { DownVote, ReturnChevron, UpVote } from './Icons'
 class EnterResult extends Component {
 	state = {
 	  nameValue: '',
@@ -26,7 +26,7 @@ class EnterResult extends Component {
 	      </button>
 	      <h2 className='mealtime'>{ this.props.mealtime }</h2>
 	      <h2 className='foodItems'>{ this.props.foodItems.name }</h2>
-	      <div className="voteButtonContainer">
+	      <div className='voteButtonContainer'>
 	        <button name='upVote' className='voteButton upVote' onClick={ this.handleClick }>
 	          <UpVote />
 	        </button>
@@ -40,29 +40,6 @@ class EnterResult extends Component {
 }
 
 export default EnterResult
-
-const UpVote = () => (
-  <div className='upVoteComponent'>
-    <div className='up1'/>
-    <div className='up'/>
-    <div className='up2'/>
-  </div>
-)
-
-const DownVote = () => (
-  <div className='downVoteComponent'>
-    <div className='down1'/>
-    <div className='down'/>
-    <div className='down2'/>
-  </div>
-)
-
-const ReturnChevron = () => (
-  <>
-    <div className='bar1' />
-    <div className='bar2' />
-  </>
-)
 
 const Container = styled.div`
 	position: absolute;
@@ -87,84 +64,15 @@ const Container = styled.div`
 		top: 0;
 		left: 0;
 		margin: 12px;
-
-		.bar1, .bar2 {
-			width: 4px;
-			height: 20px;
-			border-radius: 10px;
-			background-color: var(--primary-6);
-		}
-		.bar1 {
-			transform: translate(12px, 4.5px) rotate(50deg);
-		}
-		.bar2 {
-			transform: translate(12px, -4.5px) rotate(-50deg);
-		}
 	}
-
 	.voteButtonContainer {
 		display: flex;
 		justify-content: center;
 		width: 100%;
 		margin-top: 24px;
-
-		.voteButton {
-			margin: 0 12px;
-		}
-
-		.upVote {
-			.upVoteComponent {
-				display: flex;
-				justify-content: center;
-				width: 100%;
-				height: 100%;
-				margin-top: 5px;
-			}
-			.up1, .up2, .up {
-				width: 4px;
-				border-radius: 10px;
-				background-color: var(--primary-6);
-			}
-			.up1, .up2 {
-				height: 15px;
-			}
-			.up1 {
-				transform: rotate(35deg);
-			}
-			.up2 {
-				transform: rotate(-35deg);
-			}
-			.up {
-				height: 30px;
-			}
-		}
-
-		.downVote {
-			.downVoteComponent {
-				display: flex;
-				justify-content: center;
-				align-items: baseline;
-				width: 100%;
-				height: 100%;
-				margin-top: 5px;
-			}
-			.down1, .down2, .down {
-				width: 4px;
-				border-radius: 10px;
-				background-color: var(--primary-6);
-			}
-			.down1, .down2 {
-				height: 15px;
-			}
-			.down1 {
-				transform: rotate(-35deg);
-			}
-			.down2 {
-				transform: rotate(35deg);
-			}
-			.down {
-				height: 30px;
-			}
-		}
 	}
+	.voteButton {
+		margin: 0 12px;
+	}
+
 `
--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -10,11 +10,6 @@ const Footer = props => (
       className='button'
     >
       <Hamburger clicked={ props.clicked }/>
-      {/* <div className={ props.clicked ? `${'hamburger'} ${'clicked'}` : 'hamburger' }>
-        <div className='bar bar1' />
-        <div className='bar bar2' />
-        <div className='bar bar3' />
-      </div> */}
     </button>
   </Container>
 )
@@ -36,45 +31,4 @@ const Container = styled.div`
 		width: 50px;
 		height: 50px
 	}
-
-	/* .hamburger {
-		width: 50px;
-		height: 50px;
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-
-		.bar {
-			width: 30px;
-			height: 3px;
-			background-color: var(--primary-6);
-			margin: 4px 0;
-			border-radius: 10px;
-		}
-	}
-	.hamburger.clicked .bar1 {
-		animation: barOne 0.3s forwards
-	}
-	.hamburger.clicked .bar2 {
-		animation: fadeOut 0.5s forwards
-	}
-	.hamburger.clicked .bar3 {
-		animation: barThree 0.3s forwards
-	}
-	@keyframes barOne {
-		to {
-			transform: translate(0, 11px) rotate(45deg) ;
-		}
-	}
-	@keyframes fadeOut {
-		to {
-			opacity: 0
-		}
-	}
-	@keyframes barThree {
-		to {
-			transform: translate(0, -11px) rotate(-45deg);
-		}
-	} */
 `
