0e68637da7a9a0172416201a0408934de5ccf022
Alexander Heo <jikster712@gmail.com>
2021-03-03 11:48:13 -0800

    level1 and level2 buttons stay highlighted

--- client/components/Lists/Level2.jsx
+++ client/components/Lists/Level2.jsx
@@ -1,27 +1,45 @@
 import React, { Component } from 'react'
 
 class Level2 extends Component {
+
   render() {
+    let meal, day, overall
+    const level2 = this.props.level2
+    if (level2 === 'avgMeal') {
+		 meal = 'highlight'
+    } else if (level2 === 'avgDay') {
+		 day = 'highlight'
+    } else if (level2 === 'mostMeal') {
+		 meal = 'highlight'
+    } else if (level2 === 'leastMeal') {
+		 meal = 'highlight'
+    } else if (level2 === 'mostOverall') {
+		 overall = 'highlight'
+    } else if (level2 === 'leastOverall') {
+		 overall = 'highlight'
+    }
+
     let button = null
     if (this.props.dayOrRating === 'day') {
       button = <button
         name='byDay'
         onClick={ this.props.handleClick }
-        className='button'
+        className={`button ${day}`}
       >by Day</button>
     } else if (this.props.dayOrRating === 'rating') {
       button = <button
         name='overall'
         onClick={ this.props.handleClick }
-        className='button'
+        className={`button ${overall}`}
       >Overall</button>
     }
+
     return (
       <section className='buttonContainer level2'>
         <button
           name='level21'
           onClick={ this.props.handleClick }
-          className='button'
+          className={`button ${meal}`}
         >by Meal</button>
         {button}
       </section>
