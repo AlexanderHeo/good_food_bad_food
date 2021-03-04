0e7c771b93ae4fab7c7804869cb6ea546fd527df
Alexander Heo <jikster712@gmail.com>
2021-03-03 23:21:21 -0800

    refactored button levels to functional components

--- client/components/Lists/Level3Meal.jsx
+++ client/components/Lists/Level3Meal.jsx
@@ -1,32 +1,30 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class Level3Meal extends Component {
-  render() {
-    return (
-      <section className='buttonContainer level3'>
-        <button
-          name='meal'
-          onClick={ this.props.handleClick }
-          className='button'
-        >Breakfast</button>
-        <button
-          name='meal'
-          onClick={ this.props.handleClick }
-          className='button'
-        >Lunch</button>
-        <button
-          name='meal'
-          onClick={ this.props.handleClick }
-          className='button'
-        >Dinner</button>
-        <button
-          name='meal'
-          onClick={ this.props.handleClick }
-          className='button'
-        >Snacks</button>
-      </section>
-    )
-  }
+const Level3Meal = props => {
+  return (
+    <section className='buttonContainer level3'>
+      <button
+        name='meal'
+        onClick={ this.props.handleClick }
+        className='button'
+      >Breakfast</button>
+      <button
+        name='meal'
+        onClick={ this.props.handleClick }
+        className='button'
+      >Lunch</button>
+      <button
+        name='meal'
+        onClick={ this.props.handleClick }
+        className='button'
+      >Dinner</button>
+      <button
+        name='meal'
+        onClick={ this.props.handleClick }
+        className='button'
+      >Snacks</button>
+    </section>
+  )
 }
 
 export default Level3Meal
